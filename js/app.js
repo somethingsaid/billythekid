289b4dfa867d1bb95c6cec41007264fe564e9df5
somethingsaid <kwong.billy@gmail.com>
2016-05-02 16:58:11 -0400

    Configured some quick navigations

--- js/app.js
+++ js/app.js
@@ -1,4 +1,52 @@
 angular.module('myApp', ['ngAnimate'])
     .controller('myController', function($scope) {
+    	$scope.currentPage = null;
+    	/* Show quick Menu */
+    	$scope.showQuickMenu = function() {
+    		$scope.quickMenu = true;
+    		$scope.landing = false;
+    		$scope.aboutMe = false;
+    		$scope.myWork = false;
+    		$scope.floatNav = false;
+    	}
+
+    	/* Close Menu and return to current page */
+    	$scope.closeMenu = function() {
+    		if ($scope.currentPage === 'about') {
+    			$scope.showAboutMe();
+    		}
+    		else {
+    			$scope.showMyWork();
+    		}
+    	}
+
+    	/* Show Landing Page */
+    	$scope.showLanding = function() {
+			$scope.quickMenu = false;
+			$scope.landing = true;
+			$scope.aboutMe = false;
+			$scope.myWork = false;
+			$scope.floatNav = false;
+		}
+
+    	/* Show About Me Page */
+    	$scope.showAboutMe = function() {
+			$scope.quickMenu = false;
+			$scope.landing = false;
+			$scope.aboutMe = true;
+			$scope.myWork = false;
+			$scope.floatNav = true;
+			$scope.currentPage = 'about';
+		}
+
+    	/* Show My Work Page */
+    	$scope.showMyWork = function() {
+			$scope.quickMenu = false;
+			$scope.landing = false;
+			$scope.aboutMe = false;
+			$scope.myWork = true;
+			$scope.floatNav = true;
+			$scope.currentPage = 'work';
+		}
 
     });
