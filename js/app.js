09b24a208166f98a3b81d763b070ea8e53920c96
somethingsaid <kwong.billy@gmail.com>
2016-08-30 11:52:58 -0400

    Updated image list

--- js/app.js
+++ js/app.js
@@ -59,36 +59,41 @@ angular.module('myApp', ['ngAnimate'])
 		/* Portfolio Data, can't get gh-pages to work with data/*.json 404 error*/
 		$scope.projects = [
 		    {
-				"name": "Flickr Searcher",
-				"image": "images/project_1.jpg",
-				"description": "Search for photos by tags.  Built with AngularJS.",
-				"url": "http://somethingsaid.github.io/flickr-searcher"
+				"name": "Personal POI",
+				"image": "images/personal_poi.png",
+				"description": "Drop pins on locations of interest.  Built with MEAN.",
+				"url": "http://github.com/somethingsaid/personal-poi"
 			},
 			{
 				"name": "GeoFlicks",
-				"image": "images/project_2.jpg",
+				"image": "images/geoflicks.jpg",
 				"description": "Search for photos by location.  Utilizes Google Maps, Google Geocode, and Flickr APIs.",
 				"url": "http://somethingsaid.github.io/geoflicks"
 			},
+						{
+				"name": "Hot or Cold",
+				"image": "images/hot_or_cold.jpg",
+				"description": "A simple number guessing game",
+				"url": "http://somethingsaid.github.io/hot_or_cold"
+			},
 			{
 				"name": "Waitstaff Calculator",
-				"image": "images/project_3.jpg",
+				"image": "images/waitstaff.jpg",
 				"description": "First project with Angular directives",
 				"url": "http://somethingsaid.github.io/waitstaff-calculator"
 			},
 			{
 				"name": "JQuery Streetfighter",
-				"image": "images/project_4.jpg",
+				"image": "images/streetfighter.jpg",
 				"description": "Animate Ryu with jQuery event listeners",
 				"url": "http://somethingsaid.github.io/jquery-streetfighter/main.html"
 			},
 			{
-				"name": "Hot or Cold",
-				"image": "images/project_5.jpg",
-				"description": "A simple number guessing game",
-				"url": "http://somethingsaid.github.io/hot_or_cold"
+				"name": "Flickr Searcher",
+				"image": "images/flickr_searcher.jpg",
+				"description": "Search for photos by tags.  Built with AngularJS.",
+				"url": "http://somethingsaid.github.io/flickr-searcher"
 			}
-
 		]
 
     }]);
